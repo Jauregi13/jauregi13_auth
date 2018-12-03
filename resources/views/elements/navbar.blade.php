94266c99181f640e69730c2e825126e0e1ff422d
jauregi13 <jon.j1305@gmail.com>
2018-12-03 12:56:07 +0100

    creando menu usuario

--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -29,13 +29,13 @@
                       </div>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="{{route('login')}}" data-toggle="modal" data-target="#loginModal">
+                        <a class="nav-link" href="{{route('login')}}">
                             <i class="fa fa-sign-in"></i>
                             Login
                         </a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="{{route('register')}}" data-toggle="modal" data-target="#registerModal">
+                        <a class="nav-link" href="{{route('register')}}">
                             <i class="fa fa-user-plus"></i>
                             Registro
                         </a>
